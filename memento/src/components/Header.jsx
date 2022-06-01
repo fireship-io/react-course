52866468cc743cee7e818b1a013b7c54a317b7b9
Vladimir Livinskiy <46899094+xDamneDx@users.noreply.github.com>
2022-06-01 11:42:32 +0300

    useEffect fix
    Replaced round brackets with curly brackets.

--- memento/src/components/Header.jsx
+++ memento/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 const Header = ({ handleNewGame, wins }) => {
   // Update page title with win count
-  useEffect(() => (document.title = `${wins} wins`), [wins]);
+  useEffect(() => {document.title = `${wins} wins`}, [wins]);
 
   return (
     <header className="header">
